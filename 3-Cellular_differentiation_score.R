d8036c8734dcc0ca64e6a0ca700403162384bbcd
Huo qiang <109718652+Huo-qiang@users.noreply.github.com>
2022-07-22 16:17:16 +0800

    Update 3-Cellular_differentiation_score.R

--- 3-Cellular_differentiation_score.R
+++ 3-Cellular_differentiation_score.R
@@ -5,3 +5,24 @@ library(UCell)
 maize_sub <- readRDS("maize_sub.rds")
 dap6markergene<-read.csv("day6marker.csv")
 dap8markergene<-read.csv("day5marker.csv")
+##标记基因与单细胞检测到的基因取交集
+dap6markergene<-intersect(rownames(submaize),day6marker$day6)
+dap8markergene<-intersect(rownames(submaize),day8marker$day8)
+# 构建UCell需要输入的gene sets
+markers <- list()
+markers$dap6<-dap6markergene
+markers$dap8<-dap8markergene
+###
+maize_sub_ucell <- AddModuleScore_UCell(maize_sub, features = markers)
+cell_information <- maize_sub_ucell@meta.data
+###
+cell_information$differentiation_score<-cell_information$dap8_UCell/cell_information$dap6_UCell
+cell_information$differentiation_score<- log2(a[,22])
+maize_sub_ucell@meta.data<- cell_information
+##
+FeaturePlot(maize_sub_ucell, reduction = "umap", features = "differentiation_score", 
+            ncol = 3, order = T,
+            min.cutoff = "q03", max.cutoff = "q99",
+            cols = c("#F5F5F5", "#333399"), pt.size = 0.1)
+##
+saveRDS(maize_sub_ucell,"maize_sub_ucell.rds")
