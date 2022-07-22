32de7957e91617415efacf1b2eeb662ed10a65fa
Huo qiang <109718652+Huo-qiang@users.noreply.github.com>
2022-07-22 12:40:41 +0800

    Update 3-Cellular_differentiation_score.R

--- 3-Cellular_differentiation_score.R
+++ 3-Cellular_differentiation_score.R
@@ -1,4 +1,7 @@
 library(Seurat)
 library(ggplot2)
 library(UCell)
+##读取6dap和8dap的marker基因
 maize_sub <- readRDS("maize_sub.rds")
+dap6markergene<-read.csv("day6marker.csv")
+dap8markergene<-read.csv("day5marker.csv")
