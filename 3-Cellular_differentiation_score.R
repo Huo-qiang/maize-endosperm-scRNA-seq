5dc6b07385b02a765fd4f983042c68711d7a7d30
Huo qiang <109718652+Huo-qiang@users.noreply.github.com>
2023-04-07 13:21:20 +0800

    Update 3-Cellular_differentiation_score.R

--- 3-Cellular_differentiation_score.R
+++ 3-Cellular_differentiation_score.R
@@ -5,7 +5,7 @@ library(viridis)
 ##读取6dap和8dap的marker基因
 maize_sub <- readRDS("maize_sub.rds")
 dap6markergene<-read.csv("day6marker.csv")
-dap8markergene<-read.csv("day5marker.csv")
+dap8markergene<-read.csv("day8marker.csv")
 ##标记基因与单细胞检测到的基因取交集
 dap6markergene<-intersect(rownames(submaize),dap6markergene$day6)
 dap8markergene<-intersect(rownames(submaize),dap6markergene$day8)
